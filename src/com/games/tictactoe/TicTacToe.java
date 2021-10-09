dccd52f2e7b9a1f9c656ca5099c8251bbf7d0cd3
blackhathacker101 <justindouglas1738@gmail.com>
2021-10-08 23:17:08 -0300

    first commit

---
+++ src/com/games/tictactoe/TicTacToe.java
@@ -0,0 +1,54 @@
+package com.games.tictactoe;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class TicTacToe implements ActionListener {
+
+	Random random = new Random();
+	JFrame frame = new JFrame();
+	JPanel titlePanel = new JPanel();
+	JPanel buttonPanel = new JPanel();
+	JLabel textField = new JLabel();
+	JButton[] buttons = new JButton[9];
+	boolean player1Turn;
+	
+	
+	public TicTacToe() {
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(800, 800);
+		frame.getContentPane().setBackground(new Color(50, 50, 50));
+		frame.setLayout(new BorderLayout());
+		frame.setVisible(true);
+		
+		textField.setBackground(new Color(25, 25, 25));
+		textField.setForeground(new Color(25, 255, 0));
+		textField.setFont(new Font("Ink Free", Font.BOLD, 75));
+		textField.setHorizontalAlignment(JLabel.CENTER);
+		textField.setText("Tic-Tac-Toe");
+		textField.setOpaque(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+	}
+
+	public void firstTurn() {
+
+	}
+
+	public void check() {
+
+	}
+
+	public void xWins(int a, int b, int c) {
+
+	}
+
+	public void oWins(int a, int b, int c) {
+
+	}
+}
